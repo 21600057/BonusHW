bbcb500d7cccc426c86161cb1678825e5f7942f3
동준 <@DESKTOP-FS8K790>
2019-06-19 19:48:38 +0900

    commit

--- src/main/java/BonusHW/Main.java
+++ src/main/java/BonusHW/Main.java
@@ -13,20 +13,31 @@ public class Main
 {
 	static String CurDir;
 	static int length;
-	boolean help, reverse=false, upperCase = false, hiddenck = false;
 	static ArrayList<String> arr = new ArrayList<String>();
-
+	static ArrayList<String> revarr = new ArrayList<String>();
+	boolean help;
+	boolean reverse=false;
+	boolean upperCase = false;
+	boolean hiddenck = false;
+	
+	
 	public static void main(String[] args)
 	{
 		CurDir = System.getProperty("user.dir");
-	    System.out.println("현재 디렉토리는 " + CurDir + " 입니다");
 	    
 	    File file = new File(CurDir);
 	    File[] fileList = file.listFiles();
-        length = fileList.length;
+	    //length = fileList.length; // 전역변수에 length 길이 들어감.
+		        
         
 	    for (File tFile : fileList)
-	     arr.add(tFile.getName());
+	    {
+	    	if (tFile.isHidden() == true)
+	    		continue;
+	    	else
+	    	 arr.add(tFile.getName()); // arr에 저장다함.
+	    }
+	    length = arr.size();
 	    
 		Main Bonus = new Main();
 		Bonus.run(args);
@@ -43,6 +54,41 @@ public class Main
 					printHelp(options);
 					return;
 				}
+				
+				if (upperCase == false)
+				{
+					if (reverse == true)
+					{
+						ReverseArr(arr);
+						out(revarr);
+						System.exit(0);
+					}
+					
+					if (reverse == false)
+					 out(arr);
+					 System.exit(0);					
+				}
+
+
+				if (upperCase == true)
+				{
+					if (reverse == false)
+					{
+						optionFarr(arr,false);
+						System.exit(0);
+					}
+					
+					if (reverse == true)
+					{
+						optionFarr(revarr,true);
+						System.exit(0);
+					}					
+				}
+				
+				if (hiddenck == true)
+				{
+					ignoreHide();
+				}
 			}
 	}
 	
@@ -58,7 +104,6 @@ public class Main
 			help = cmd.hasOption("h");
 			reverse = cmd.hasOption("r");
 			upperCase = cmd.hasOption("F");
-			
 
 		} catch (Exception e) 
 		
@@ -106,9 +151,85 @@ public class Main
 		String footer ="\nBonusHW program!!";
 		formatter.printHelp("BonusHW", header, options, footer, true);
 	}
-	
-	public ArrayList<String> getArr(ArrayList<String> value)
-	{
 		
+	public ArrayList<String> ReverseArr(ArrayList<String> array)
+	{
+		ArrayList<String> reverseArr = new ArrayList<String>();
+		
+		for (int i=length-1; i>=0; i--)
+		{
+			revarr.add(array.get(i));
+			reverseArr.add(array.get(i));
+		}
+		
+		return reverseArr;
 	}
+	
+	public void out(ArrayList<String> outarr)
+	{
+		for (int i=0; i<length; i++)
+		 System.out.println(outarr.get(i));
+	}
+	
+	public ArrayList<String> optionFarr(ArrayList<String> oarr, boolean checker)
+	{
+		ArrayList<String> st = new ArrayList<String>();
+		
+	    File file = new File(CurDir);
+		File[] fileList = file.listFiles();
+		
+		if (checker == false)
+		{
+			for(File tFile : fileList)
+		    {
+				if(tFile.isDirectory()) // 디렉토리라면 
+				 st.add(tFile.getName() + "/");
+				else // 파일이라면 
+				{
+					if (tFile.canExecute() == true) // 실행가능한 파일이면 *
+					 st.add(tFile.getName() + "*");
+					else
+					 st.add(tFile.getName());
+				}
+		    }				
+			for (int i=0; i<length; i++)
+				 System.out.println(st.get(i));
+		
+		}
+		
+		if (checker == true)
+		{
+			for(File tFile : fileList)
+		    {
+				if(tFile.isDirectory()) // 디렉토리라면 
+				 st.add(tFile.getName() + "/");
+				else // 파일이라면 
+				{
+					if (tFile.canExecute() == true) // 실행가능한 파일이면 *
+					 st.add(tFile.getName() + "*");
+					else
+					 st.add(tFile.getName());
+				}
+		    }
+			
+			for (int i=length-1; i>=0; i--)
+			 System.out.println(st.get(i));
+		}
+		
+	return st;
+	
+	}
+	
+	public void ignoreHide()
+	{
+	    File file = new File(CurDir);
+		File[] fileList = file.listFiles();
+		
+		for(File tFile : fileList)
+		{
+			if ((tFile.isHidden() == true) || (tFile.isHidden() == false))
+			 System.out.println(tFile.getName());			
+		}
+	}
+
 }
