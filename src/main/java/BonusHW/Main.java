afe52e115fc3cb48c364a2980aa7a157eda9fe09
동준 <@DESKTOP-FS8K790>
2019-06-19 14:28:47 +0900

    commit

--- src/main/java/BonusHW/Main.java
+++ src/main/java/BonusHW/Main.java
@@ -6,34 +6,60 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import java.io.File;
 
-public class Main 
+public class Main
 {
-	String path;
-	boolean help;
+	static String CurDir;
+	static String path;
+	boolean help, reverse=false;
 
 	public static void main(String[] args)
 	{
-		System.out.println("진입부");
+		CurDir = System.getProperty("user.dir");
+	    System.out.println("현재 디렉토리는 " + CurDir + " 입니다");
+	    
 		Main Bonus = new Main();
 		Bonus.run(args);
 	}
 	
 	public void run(String[] args)
-	{
-		
-		System.out.println("ㅎㅎ");
-		
-		Options options = createOptions();
-		
-		if(parseOptions(options, args))
-		{
-			if (help)
+	{			
+			Options options = createOptions();
+			
+			if(parseOptions(options, args))
 			{
-				printHelp(options);
-				return;
+				if (help)
+				{
+					printHelp(options);
+					return;
+				}
+				
+			    File file = new File(CurDir);
+			    File[] fileList = file.listFiles();
+	            int length = fileList.length;
+	            
+			    if (reverse == true)
+				{
+			    	if(file.isDirectory()) 
+				    {
+			    		for (int i=length-1; i>0; i--)
+		            	 System.out.println(fileList[i].getName());
+				    }
+			    	else
+			    	 System.out.println("It is not a directory.");
+				}
+			    if (reverse == false)
+			    {
+			    	if(file.isDirectory()) 
+				    {
+			    		for(File tFile : fileList) 
+				         System.out.println(tFile.getName());
+				    }
+			    	else
+			    	 System.out.println("It is not a directory.");
+			    }
 			}
-		}
 	}
 	
 	public boolean parseOptions(Options options, String[] args) 
@@ -46,6 +72,7 @@ public class Main
 
 			path = cmd.getOptionValue("p");
 			help = cmd.hasOption("h");
+			reverse = cmd.hasOption("r");
 
 		} catch (Exception e) 
 		
@@ -66,23 +93,27 @@ public class Main
 				.desc("Set a path of a directory or a file to display")
 				.hasArg()
 				.argName("Path name to display")
-				.required()
 				.build());
 	
 		// add options by using OptionBuilder
 		options.addOption(Option.builder("h").longOpt("help")
 		        .desc("Help")
 		        .build());
+		
+		options.addOption(Option.builder("r").longOpt("reverse")
+		        .desc("reverse Print")
+				.argName("reverse name print")
+		        .build());
 
 		return options;
 	}
 	
 	private void printHelp(Options options) 
-	{
+	{/*
 		// automatically generate the help statement
 		HelpFormatter formatter = new HelpFormatter();
 		String header = "BonusHW program!!";
 		String footer ="\nBonusHW program!!";
-		formatter.printHelp("BonusHW", header, options, footer, true);
+		formatter.printHelp("BonusHW", header, options, footer, true);*/
 	}
 }
